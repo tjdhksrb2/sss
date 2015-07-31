1f1bc1196b89ae21ac4a790fe4a0f47b35373518
서완규 <tjdhksrb2@naver.com>
2015-07-31 13:56:50 +0900

    Create firstfactorial.rb

---
+++ firstfactorial.rb
@@ -0,0 +1,14 @@
+def FirstFactorial(num)
+
+  # code goes here
+  while ( num = 0)
+    fac = num*(num-1)
+
+  end
+  
+  return fac
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+FirstFactorial(STDIN.gets)  
